f6125a4e1e26b6e61390d3f281cd75f5cb135001
Bilikto Dambaev <dambaevbilikto@gmail.com>
2024-02-05 23:53:38 +0300

    Synchronized Tracking and Registry URI

--- src/models/register.py
+++ src/models/register.py
@@ -2,7 +2,6 @@ import hydra
 from omegaconf import DictConfig
 
 import mlflow
-from mlflow import MlflowClient
 
 
 @hydra.main(version_base=None, config_path="../conf", config_name="train")
@@ -11,7 +10,9 @@ def register_best_model(config: DictConfig):
     optimized_metric = config["optimized_metric"]
 
     remote_server_uri = config["mlflow_config"]["remote_server_uri"]
+    remote_registry_uri = config["mlflow_config"]["mlflow_registry_uri"]
     mlflow.set_tracking_uri(remote_server_uri)
+    mlflow.set_registry_uri(remote_registry_uri)
 
     runs = mlflow.search_runs(
         experiment_names=[model_name],
@@ -22,17 +23,6 @@ def register_best_model(config: DictConfig):
     run_id = best_run["run_id"]
     model_uri = f"runs:/{run_id}/model"
     mlflow.register_model(model_uri, model_name, await_registration_for=0)
-
-    # Register model name in the model registry
-    client = MlflowClient()
-    print(dict(client.search_model_versions(f"name='{model_name}'")[0])["version"])
-    # client.create_registered_model(model_name)
-
-    # # Create a new version of the rfr model under the registered model name
-    # run_id = best_run["run_id"]
-    # model_uri = f"runs:/{run_id}/model"
-    # model_src = RunsArtifactRepository.get_underlying_uri(model_uri)
-    # client.create_model_version(model_name, model_src, run_id)
 
 
 if __name__ == "__main__":
